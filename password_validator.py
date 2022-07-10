e7007b4cd9d3e3f0d548ad91866ca0458cf0873a
4alex43 <51155496+4alex43@users.noreply.github.com>
2022-07-10 22:11:09 +0300

    Update password_validator.py
    remove if for Which checks if there is a special character or space & add comments

--- password_validator.py
+++ password_validator.py
@@ -1,12 +1,17 @@
-from string import ascii_letters, digits
 import sys, os
-
-
 # Password validation in Python
-# using naive method
-
 # Function to validate the password
 def password_check(passwd):
+    """
+        check if have:
+        have equal or bigger digits than 10
+        Last 1 upperCase
+        Last 1 lowerCase
+        Last 1 digit(number)
+        in the password
+        :param passwd: str
+        :return: return 0(valid)(green)/1(invalid and error comments)(red)
+    """
     val = 0
     if len(passwd) < 10:
         print("length should be bigger then 10 digits")
@@ -23,10 +28,7 @@ def password_check(passwd):
     if not any(char.islower() for char in passwd):
         print("Password should have at least one lowercase letter")
         val = 1
-#remove dont ask for it
-    if set(passwd).difference(ascii_letters + digits):
-        print("Password has special characters.")
-        val = 1
+        
     return val
 
 
@@ -34,23 +36,21 @@ def password_check(passwd):
 def main():
     os.system("")
     res =""
-    if sys.argv[1] == "-f":
+    if sys.argv[1] == "-f":#If the option "-f" is added the password should be retrieved from a file
         if len(sys.argv) == 2:
             print("Error don't have a path")
-        else:
+        else:#read password from a file
             f = open(sys.argv[2], 'r')
-
             res = password_check(f.readline())
             f.close()
     else:
         res = password_check(sys.argv[1])
 
     if (res == 0):
-#Dont need in the h.w        #print(f"{str(sys.argv[1])}\n",file=open('password.txt', 'a+'))#add string to file in new line everyTime
-        print('\033[0;32m' + str(res) + '\033[0m')
+        print('\033[0;32m' + str(res) + '\033[0m')#color green
     else:
 
-        print('\033[91m' + str(res) + '\033[0m')
+        print('\033[91m' + str(res) + '\033[0m')#color red
 
 
 main()
